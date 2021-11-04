7542688fe5367fda9037cf7d2c2f7e314b8eea33
yasminm1 <60303667+yasminm1@users.noreply.github.com>
2021-11-04 13:09:58 -0400

    ARDemo

--- ARDemo/ViewController.swift
+++ ARDemo/ViewController.swift
@@ -12,6 +12,8 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var videoNode: SKVideoNode!
+    var videoPlayer: AVPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,13 +32,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+    super.viewWillAppear(animated)
+    // Create a session configuration
+    let configuration = ARImageTrackingConfiguration()
+    guard let arImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else { return }
+    configuration.trackingImages = arImages
+    // Run the view's session
+    sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -56,6 +58,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    guard anchor is ARImageAnchor else { return }
+    guard let referenceImage = ((anchor as? ARImageAnchor)?.referenceImage) else { return }
+    guard let container = sceneView.scene.rootNode.childNode(withName: "container", recursively: false) else { return }
+    container.removeFromParentNode()
+    node.addChildNode(container)
+    container.isHidden = false
+    guard let videoURL = Bundle.main.url(forResource: "video", withExtension: ".mp4") else { return }
+    videoPlayer = AVPlayer(url: videoURL)
+    let videoScene = SKScene(size: CGSize(width: 720.0, height: 1280.0))
+    videoNode = SKVideoNode(avPlayer: videoPlayer)
+    videoNode.position = CGPoint(x: videoScene.size.width/2, y: videoScene.size.height/2)
+    videoNode.size = videoScene.size
+    videoNode.yScale = -1
+    videoNode.play()
+    videoScene.addChild(videoNode)
+    guard let video = container.childNode(withName: "video", recursively: true)
+        else { return }
+        video.geometry?.firstMaterial?.diffuse.contents = videoScene
+    video.scale = SCNVector3(x: Float(referenceImage.physicalSize.width), y: Float(referenceImage.physicalSize.height), z: 1.0)
+    video.position = node.position
+    // For Animation
+    guard let videoContainer = container.childNode(withName: "videoContainer", recursively: false) else { return }
+    videoContainer.runAction(SCNAction.sequence([SCNAction.wait(duration: 1.0), SCNAction.scale(to: 1.0, duration: 0.5)]))
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
